cfeb2a775a804bafb5f606647f8cd7034dc4ee87
Mathew Dony <mathew.dony@jaxsta.com>
2022-03-09 19:37:45 +1100

    Clean up some boilerplate

--- mathew-dony-portfolio/pages/index.js
+++ mathew-dony-portfolio/pages/index.js
@@ -1,42 +1,32 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { Header } from 'components'
+import Head from "next/head";
+import Image from "next/image";
+import { Header } from "components";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-    <Header />
+    <div>
+      <Header />
       <Head>
         <title>Mathew Dony</title>
         <meta name="description" content="Portfolio of Mathew Dony" />
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <main>
+        <h1>
+          Welcome to my portfolio!
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+        <p>
+          <code>This is the start of an editing session</code>
         </p>
-
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+      <footer>
+        <span>
+          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        </span>
       </footer>
     </div>
-  )
+  );
 }
