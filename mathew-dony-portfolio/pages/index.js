a3f3292856ccb7d67d81a25b0a056ffb6981a766
Mathew Dony <mathew.dony@jaxsta.com>
2022-03-09 19:23:56 +1100

    Add absolute path,import Header

--- mathew-dony-portfolio/pages/index.js
+++ mathew-dony-portfolio/pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Header } from 'components'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+    <Header />
       <Head>
         <title>Mathew Dony</title>
         <meta name="description" content="Portfolio of Mathew Dony" />
