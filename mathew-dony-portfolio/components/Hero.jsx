b3274a586c83a3b44ab194c7f77618da5e70b024
Mathew Dony <mathew.dony@jaxsta.com>
2022-03-14 17:47:36 +1100

    Added Framer animation

--- mathew-dony-portfolio/components/Hero.jsx
+++ mathew-dony-portfolio/components/Hero.jsx
@@ -1,11 +1,15 @@
-import React, { useRef } from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
+import { animate, stagger } from "motion";
 
 const Hero = () => {
+  useEffect(() => {
+    animate(".click", { transform: "scale(0.5)" }, { duration: 3.5 });
+  });
   return (
     <HeroContainer>
       <main>
-        <h1>Hi!</h1>
+        <h1 className="click">Hi!</h1>
         <h2>I'm Mathew Dony</h2>
         <p>
           <code>Software Engineer</code>
