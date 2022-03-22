aa201d433d0643ab0e0f28460960cfcd0a84d0a2
Mathew Dony <mathewdony007@gmail.com>
2022-03-22 23:16:31 +1100

    Added scrollable project tiles

--- mathew-dony-portfolio/components/ProjectTile.jsx
+++ mathew-dony-portfolio/components/ProjectTile.jsx
@@ -5,10 +5,26 @@ import { projectData } from "./constants/ProjectData";
 import styled from "styled-components";
 
 const ProjectTile = () => {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    // console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth + 16);
+  }, []);
+
   return (
     <TileContainer>
-      <motion.h1 className="carousel ">
-        <motion.div className="inner-carousel">
+      <motion.div
+        ref={carousel}
+        className="carousel"
+        whileTap={{ cursor: "grabbing" }}
+      >
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          className="inner-carousel"
+        >
           {projectData.map((project, index) => {
             return (
               <motion.div className="item">
@@ -17,17 +33,32 @@ const ProjectTile = () => {
             );
           })}
         </motion.div>
-      </motion.h1>
+      </motion.div>
     </TileContainer>
   );
 };
 
 const TileContainer = styled.div`
   .item {
-    height: 5rem;
-    width: 12rem;
-    background-color: blue;
-    border-radius: 0.8rem;
+    height: 35vh;
+    min-width: 50vw;
+    background-color: #104f72;
+    border-radius: 2rem;
+    padding: 2rem;
+    margin: 1rem;
+  }
+
+  p {
+    font-size: 1rem;
+  }
+
+  .inner-carousel {
+    display: flex;
+  }
+
+  .carousel {
+    cursor: grab;
+    overflow: hidden;
   }
 `;
 
