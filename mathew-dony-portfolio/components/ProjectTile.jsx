256df054d353b4abf809a537cbf319390da74e79
Mathew Dony <mathewdony007@gmail.com>
2022-03-22 22:28:14 +1100

    Add project tiles

--- mathew-dony-portfolio/components/ProjectTile.jsx
+++ mathew-dony-portfolio/components/ProjectTile.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { useRef, useEffect, useState } from "react";
+import { projectData } from "./constants/ProjectData";
+import styled from "styled-components";
 
 const ProjectTile = () => {
   return (
-    <div>ProjectTile</div>
-  )
-}
+    <TileContainer>
+      <motion.h1 className="carousel ">
+        <motion.div className="inner-carousel">
+          {projectData.map((project, index) => {
+            return (
+              <motion.div className="item">
+                <p>{project.name}</p>
+              </motion.div>
+            );
+          })}
+        </motion.div>
+      </motion.h1>
+    </TileContainer>
+  );
+};
 
-export default ProjectTile
+const TileContainer = styled.div`
+  .item {
+    height: 5rem;
+    width: 12rem;
+    background-color: blue;
+    border-radius: 0.8rem;
+  }
+`;
+
+export default ProjectTile;
