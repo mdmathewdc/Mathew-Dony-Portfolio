e3667035e404a7a886a3974875827ab728057dfd
Mathew Dony <mathewdony007@gmail.com>
2022-03-29 20:46:03 +1100

    Add 2 og image tags

--- mathew-dony-portfolio/pages/index.js
+++ mathew-dony-portfolio/pages/index.js
@@ -11,6 +11,7 @@ const Home = () => {
         <meta name="description" content="Portfolio of Mathew Dony" />
         <link rel="icon" href="/favicon.png" />
         <meta property="og:image" content="https://artwork.jaxsta.com/995/MerebaMusicKU.jpg?d=500x500" />
+        <meta property="og:image" content="https://artwork.jaxsta.com/995/MerebaMusic.jpg?d=500x500" />
       </Head>
 
       <Hero />
