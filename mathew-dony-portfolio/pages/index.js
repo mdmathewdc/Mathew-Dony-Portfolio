4bd82476963bd9db4712d8444899c13b7c44f312
Mathew Dony <mathewdony007@gmail.com>
2022-03-29 21:14:37 +1100

    og

--- mathew-dony-portfolio/pages/index.js
+++ mathew-dony-portfolio/pages/index.js
@@ -10,7 +10,7 @@ const Home = () => {
         <title>Mathew Dony</title>
         <meta name="description" content="Portfolio of Mathew Dony" />
         <link rel="icon" href="/favicon.png" />
-        <meta property="og:image" content="https://artwork.jaxsta.com/995/MerebaMusicKU.jpg?d=500x500" />
+        <meta property="og:image" content="https://artwork.jaxsta.com/995/MerebaMusic.jpg?d=500x500" />
         <meta property="og:image" content="https://artwork.jaxsta.com/assets/images/og/large-social-image.png" />
       </Head>
 
