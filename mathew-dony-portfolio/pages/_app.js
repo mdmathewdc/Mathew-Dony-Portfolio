253b566ec3c36535d771057c047d77159d1f5522
Mathew Dony <mathewdony007@gmail.com>
2022-09-19 14:59:50 +1000

    Add google analytics

--- mathew-dony-portfolio/pages/_app.js
+++ mathew-dony-portfolio/pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-8058SPB8F5`}
+      />
+
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8058SPB8F5', {
+              page_path: window.location.pathname,
+            });
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
