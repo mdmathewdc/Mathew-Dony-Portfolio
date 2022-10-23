fea5354954822cb8f50d4100bd4bb4c38a1a7d74
mdmathewdc <mathewdony007@gmail.com>
2022-10-23 23:12:44 +1100

    Add hygraph as CMS

--- mathew-dony-portfolio/components/Resume.jsx
+++ mathew-dony-portfolio/components/Resume.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { SectionHeader } from "./CommonStyles";
 import { JobTab } from ".";
-import { jobData } from "./constants/JobData";
 import Fade from "react-reveal/Fade";
+import { HYGRAPH_PERMANENTAUTH_TOKEN, HYGRAPH_URL } from "./constants/Hygraph";
 
 const Resume = () => {
+  const [jobData, setJobData] = useState(null);
+
+  useEffect(() => {
+    const fetchJobData = async () => {
+      const headers = {
+        "content-type": "application/json",
+        Authorization: `Bearer ${HYGRAPH_PERMANENTAUTH_TOKEN}`,
+      };
+      const requestBody = {
+        query: `{
+          jobs {
+            name
+            role
+            time
+            url
+            description
+          }
+        }`,
+      };
+      const options = {
+        method: "POST",
+        headers,
+        body: JSON.stringify(requestBody),
+      };
+      const response = await (await fetch(HYGRAPH_URL, options)).json();
+      setJobData(response?.data?.jobs);
+    };
+
+    fetchJobData();
+  }, []);
   return (
     <Fade big duration={1500} ssrReveal>
       <ResumeContainer id="resume-section">
@@ -13,7 +43,7 @@ const Resume = () => {
           <h2>Where I've Worked</h2>
           <span></span>
         </SectionHeader>
-        <JobTab data={jobData} />
+        {jobData && <JobTab data={jobData} />}
       </ResumeContainer>
     </Fade>
   );
